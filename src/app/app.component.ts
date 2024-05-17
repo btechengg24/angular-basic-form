1481184dec344761b53c3dc3a58ca9a159e8c938
btechengg24 <btechengg24@gmail.com>
2024-05-17 16:46:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
+// app.component.ts
+
 import { Component } from '@angular/core';
+
+// FormBuilder to create form and Validators for validations
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-1';
+  constructor(private fb: FormBuilder) {}
+
+  userForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    phoneNo: ['', Validators.required],
+    password: [''],
+    confirmPassword: [''],
+  });
+
+  addUser() {
+    console.log(this.userForm.value);
+    this.userForm.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+// app.module.ts
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// Reactive form for generate and validate the form
+import { ReactiveFormsModule } from '@angular/forms';
+
+// primeng components
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule, // <- import here
+    InputTextModule, // <- import here
+    ButtonModule, // <- import here
+    CardModule, // <- import here
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
